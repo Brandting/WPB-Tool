36364764c2ae2262e9572f5b7559883b27af9590
Brandting <arbeit@joernbrandt.com>
2026-04-14 20:04:27 +0200

    NEW1
    New

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'bauleiter-field-v1';
+const CACHE = 'bauleiter-field-v2';
 const FILES = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -11,7 +11,12 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
+  // Network first, fallback to cache
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(()=>cached))
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
